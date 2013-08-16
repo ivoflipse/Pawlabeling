4088a247ba161f984a6846a94b5d812a08df8b1c
Ivo Flipse <ivoflipse@gmail.com>
2013-08-16 13:43:10 +0200

    Moved the tests to a subfolder

--- pawlabeling/functions/calculations.py
+++ pawlabeling/functions/calculations.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pawlabeling.settings import configuration
 
+
 def interpolate_time_series(data, length=100):
     """
     Interpolate time series expects a 1D array
@@ -10,9 +11,10 @@ def interpolate_time_series(data, length=100):
     from scipy import interpolate
     x = np.arange(0, len(data))
     f = interpolate.interp1d(x, data, bounds_error=False)
-    x_new = np.linspace(0, len(data)-1, num=length)
+    x_new = np.linspace(0, len(data) - 1, num=length)
     data_new = f(x_new)
     return data_new
+
 
 def calculate_cop(data, version="scipy"):
     assert len(data.shape) == 3
@@ -20,6 +22,7 @@ def calculate_cop(data, version="scipy"):
         return calculate_cop_scipy(data)
     elif version == "numpy":
         return calculate_cop_numpy(data)
+
 
 def calculate_cop_numpy(data):
     y, x, z = np.shape(data)
@@ -30,7 +33,7 @@ def calculate_cop_numpy(data):
     temp_x, temp_y = np.zeros(y), np.zeros(x)
     for frame in range(z):
         #print frame, np.sum(data[:, :, frame])
-        if np.sum(data[:, :, frame]) > 0.0: # Else divide by zero
+        if np.sum(data[:, :, frame]) > 0.0:  # Else divide by zero
             # This can be rewritten as a vector calculation
             for col in range(y):
                 temp_x[col] = np.sum(data[col, :, frame] * x_coordinate)
@@ -41,19 +44,21 @@ def calculate_cop_numpy(data):
             cop_y[frame] = np.divide(np.sum(temp_y), np.sum(data[:, :, frame]))
     return cop_x, cop_y
 
+
 def calculate_cop_scipy(data):
     from scipy.ndimage.measurements import center_of_mass
     y, x, z = np.shape(data)
     cop_x = np.zeros(z, dtype=np.float32)
     cop_y = np.zeros(z, dtype=np.float32)
     for frame in range(z):
-        if np.sum(data[:,:,frame]) > 0:
+        if np.sum(data[:, :, frame]) > 0:
             # While it may seem odd, x and y are mixed up, must be my own fault
             y, x = center_of_mass(data[:, :, frame])
             # This used to say + 1, but I can't image that's necessary
             cop_x[frame] = x
             cop_y[frame] = y
     return cop_x, cop_y
+
 
 def force_over_time(data):
     """
@@ -70,10 +75,12 @@ def pixel_count_over_time(data):
     x, y, z = data.shape
     return np.array([np.count_nonzero(data[:, :, frame]) for frame in range(z)])
 
+
 def surface_over_time(data):
     assert len(data.shape) == 3
     pixel_counts = pixel_count_over_time(data)
     return[p_c * configuration.sensor_surface for p_c in pixel_counts]
+
 
 def pressure_over_time(data):
     assert len(data.shape) == 3
@@ -81,5 +88,3 @@ def pressure_over_time(data):
     pixel_counts = pixel_count_over_time(data)
     surface = [p_c * configuration.sensor_surface for p_c in pixel_counts]
     return np.divide(force, surface)
-
-
