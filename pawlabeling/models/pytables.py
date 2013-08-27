cf30564207c407174eef901cc34448717e26a5de
Ivo Flipse <ivoflipse@gmail.com>
2013-08-27 22:53:55 +0200

    Just in case I forget anything

--- pawlabeling/models/pytables.py
+++ pawlabeling/models/pytables.py
@@ -26,67 +26,6 @@ def actual_kwargs():
     return decorator
 
 
-class Subjects(tables.IsDescription):
-    subject_id = tables.StringCol(64)
-    first_name = tables.StringCol(32)
-    last_name = tables.StringCol(32)
-    address = tables.StringCol(32)
-    city = tables.StringCol(32)
-    postal_code = tables.StringCol(32)
-    phone = tables.StringCol(32)
-    email = tables.StringCol(32)
-    birthday = tables.StringCol(32)
-    mass = tables.Float32Col()
-
-
-class Sessions(tables.IsDescription):
-    session_id = tables.StringCol(64)
-    subject_id = tables.StringCol(64)
-    session_name = tables.StringCol(32)
-    date = tables.StringCol(32)
-    time = tables.StringCol(32)
-
-# Store session labels as a key/value store
-# You can have multiple labels per session this way
-class SessionLabels(tables.IsDescription):
-    session_id = tables.StringCol(64)
-    session_label = tables.StringCol(64)
-
-
-class Measurements(tables.IsDescription):
-    measurement_id = tables.StringCol(64)
-    session_id = tables.StringCol(64)
-    subject_id = tables.StringCol(64)
-    measurement_name = tables.StringCol(64)
-    number_of_frames = tables.UInt32Col()
-    number_of_rows = tables.UInt32Col()
-    number_of_cols = tables.UInt32Col()
-    measurement_frequency = tables.UInt32Col()
-    brand = tables.StringCol(32)
-    model = tables.StringCol(32)
-    date = tables.StringCol(32)
-    time = tables.StringCol(32)
-
-
-class Contacts(tables.IsDescription):
-    measurement_id = tables.StringCol(64)
-    session_id = tables.StringCol(64)
-    subject_id = tables.StringCol(64)
-    contact_id = tables.UInt16Col()
-    label = tables.UInt16Col()
-    min_x = tables.UInt16Col()
-    max_x = tables.UInt16Col()
-    min_y = tables.UInt16Col()
-    max_y = tables.UInt16Col()
-    min_z = tables.UInt16Col()
-    max_z = tables.UInt16Col()
-    width = tables.UInt16Col()
-    height = tables.UInt16Col()
-    length = tables.UInt16Col()
-    invalid = tables.BoolCol()
-    filtered = tables.BoolCol()
-
-
 def main():
     # Open in append mode
     with tables.openFile("data.h5", mode="a", title="Data") as h5file:
@@ -166,25 +105,23 @@ class Table(object):
 
 
 class SubjectsTable(Table):
-    """
-        The Subjects Table consist out of:
-            subject_id = tables.StringCol(64)
-            first_name = tables.StringCol(32)
-            last_name = tables.StringCol(32)
-            address = tables.StringCol(32)
-            city = tables.StringCol(32)
-            postal_code = tables.StringCol(32)
-            phone = tables.StringCol(32)
-            email = tables.StringCol(32)
-            birthday = tables.StringCol(32)
-            mass = tables.Float32Col()
-    """
+    class Subjects(tables.IsDescription):
+        subject_id = tables.StringCol(64)
+        first_name = tables.StringCol(32)
+        last_name = tables.StringCol(32)
+        address = tables.StringCol(32)
+        city = tables.StringCol(32)
+        postal_code = tables.StringCol(32)
+        phone = tables.StringCol(32)
+        email = tables.StringCol(32)
+        birthday = tables.StringCol(32)
+        mass = tables.Float32Col()
 
     def __init__(self):
         super(SubjectsTable, self).__init__()
         # Check if table has subjects table
         if 'subjects' not in self.table.root:
-            self.subjects_table = self.table.createTable(where="/", name="subjects", description=Subjects,
+            self.subjects_table = self.table.createTable(where="/", name="subjects", description=SubjectsTable.Subjects,
                                                          title="Subjects")
 
     def create_subject(self, **kwargs):
@@ -206,15 +143,16 @@ class SubjectsTable(Table):
 
 
 class SessionsTable(Table):
-    """
-        The Sessions Table consist out of:
-            session_id = tables.StringCol(64)
-            subject_id = tables.StringCol(64)
-            session_name = tables.StringCol(32)
-            date = tables.StringCol(32)
-            time = tables.StringCol(32)
-        For each session_id a new group is created
-    """
+    class Sessions(tables.IsDescription):
+        session_id = tables.StringCol(64)
+        subject_id = tables.StringCol(64)
+        session_name = tables.StringCol(32)
+        date = tables.StringCol(32)
+        time = tables.StringCol(32)
+
+    class SessionLabels(tables.IsDescription):
+        session_id = tables.StringCol(64)
+        session_label = tables.StringCol(64)
 
     def __init__(self, subject_id):
         super(SessionsTable, self).__init__()
@@ -222,8 +160,10 @@ class SessionsTable(Table):
         self.subject_group = self.table.root.__getattr__(self.subject_id)
 
         if 'sessions' not in self.subject_group:
-            self.table.createTable(where=self.subject_group, name="sessions", description=Sessions, title="Sessions")
-            self.table.createTable(where=self.subject_group, name="session_labels", description=Sessions,
+            self.table.createTable(where=self.subject_group, name="sessions", description=SessionsTable.Sessions,
+                                   title="Sessions")
+            self.table.createTable(where=self.subject_group, name="session_labels",
+                                   description=SessionsTable.SessionLabels,
                                    title="Session Labels")
 
     def create_session(self, **kwargs):
@@ -240,24 +180,21 @@ class SessionsTable(Table):
         self.create_group(parent=self.subject_group, item_id=session_id)
 
 
+# TODO: Figure out how to prevent duplicate entries
 class MeasurementsTable(Table):
-    """
-        Description of the measurements table:
-            measurement_id = tables.StringCol(64)
-            session_id = tables.StringCol(64)
-            subject_id = tables.StringCol(64)
-            measurement_name = tables.StringCol(64)
-            data_id = tables.StringCol(64)
-            number_of_frames = tables.UInt32Col()
-            number_of_rows = tables.UInt32Col()
-            number_of_cols = tables.UInt32Col()
-            measurement_frequency = tables.UInt32Col()
-            brand = tables.StringCol(32)
-            model = tables.StringCol(32)
-            date = tables.StringCol(32)
-            time = tables.StringCol(32)
-        The actual data is stored in the MeasurementsGroup under the data_id
-    """
+    class Measurements(tables.IsDescription):
+        measurement_id = tables.StringCol(64)
+        session_id = tables.StringCol(64)
+        subject_id = tables.StringCol(64)
+        measurement_name = tables.StringCol(64)
+        number_of_frames = tables.UInt32Col()
+        number_of_rows = tables.UInt32Col()
+        number_of_cols = tables.UInt32Col()
+        measurement_frequency = tables.UInt32Col()
+        brand = tables.StringCol(32)
+        model = tables.StringCol(32)
+        date = tables.StringCol(32)
+        time = tables.StringCol(32)
 
     def __init__(self, subject_id, session_id):
         super(MeasurementsTable, self).__init__()
@@ -268,7 +205,7 @@ class MeasurementsTable(Table):
 
         if 'measurements' not in self.session_group:
             self.measurements_table = self.table.createTable(where=self.session_group, name="measurements",
-                                                             description=Measurements,
+                                                             description=MeasurementsTable.Measurements,
                                                              title="Measurements")
 
     def create_measurement(self, **kwargs):
@@ -284,42 +221,24 @@ class MeasurementsTable(Table):
         self.create_group(parent=self.session_group, item_id=measurement_id)
 
 
-class DataTable(Table):
-    def __init__(self, subject_id, session_id):
-        super(DataTable, self).__init__()
-        self.subject_id = subject_id
-        self.session_id = session_id
-        self.session_group = self.table.root.__getattr__(self.subject_id).__getattr__(self.session_id)
-
-    def create_data(self, measurement_id, data):
-        measurement_group = self.session_group.__getattr__(measurement_id)
-        atom = tables.Atom.from_dtype(data.dtype)
-        filters = tables.Filters(complib="blosc", complevel=9)
-        data_array = self.table.createCArray(where=measurement_group, name=measurement_id,
-                                             atom=atom, shape=data.shape, filters=filters)
-        data_array[:] = data
-
-
 class ContactsTable(Table):
-    """
-        Description of the contacts table:
-            measurement_id = tables.StringCol(64)
-            session_id = tables.StringCol(64)
-            subject_id = tables.StringCol(64)
-            contact_id = tables.UInt16Col()
-            label = tables.UInt16Col()
-            min_x = tables.UInt16Col()
-            max_x = tables.UInt16Col()
-            min_y = tables.UInt16Col()
-            max_y = tables.UInt16Col()
-            min_z = tables.UInt16Col()
-            max_z = tables.UInt16Col()
-            width = tables.UInt16Col()
-            height = tables.UInt16Col()
-            length = tables.UInt16Col()
-            invalid = tables.BoolCol()
-            filtered = tables.BoolCol()
-    """
+    class Contacts(tables.IsDescription):
+        measurement_id = tables.StringCol(64)
+        session_id = tables.StringCol(64)
+        subject_id = tables.StringCol(64)
+        contact_id = tables.UInt16Col()
+        label = tables.UInt16Col()
+        min_x = tables.UInt16Col()
+        max_x = tables.UInt16Col()
+        min_y = tables.UInt16Col()
+        max_y = tables.UInt16Col()
+        min_z = tables.UInt16Col()
+        max_z = tables.UInt16Col()
+        width = tables.UInt16Col()
+        height = tables.UInt16Col()
+        length = tables.UInt16Col()
+        invalid = tables.BoolCol()
+        filtered = tables.BoolCol()
 
     def __init__(self, subject_id, session_id, measurement_id):
         super(ContactsTable, self).__init__()
@@ -331,7 +250,7 @@ class ContactsTable(Table):
 
         if 'contacts' not in self.measurement_group:
             self.contacts_table = self.table.createTable(where=self.measurement_group, name="contacts",
-                                                         description=Contacts,
+                                                         description=ContactsTable.Contacts,
                                                          title="Contacts")
 
     def create_contact(self, **kwargs):
@@ -346,43 +265,13 @@ class ContactsTable(Table):
         self.create_row(self.contacts_table, **kwargs)
         self.create_group(parent=self.measurement_group, item_id=contact_id)
 
-def store_contact_data(table, contact_group, contact_id, data):
+
+# This function can be used for data, contact_data and normalized_contact_data
+# Actually also for all the different results (at least the time series)
+def store_data(table, group, item_id, data):
     atom = tables.Atom.from_dtype(data.dtype)
     filters = tables.Filters(complib="blosc", complevel=9)
-    data_array = table.createCArray(where=contact_group, name=contact_id,
-                                         atom=atom, shape=data.shape, filters=filters)
+    data_array = table.createCArray(where=group, name=item_id,
+                                    atom=atom, shape=data.shape, filters=filters)
     data_array[:] = data
-
-class ContactDataTable(Table):
-    def __init__(self, subject_id, session_id, measurement_id, contact_id):
-        super(ContactDataTable, self).__init__()
-        self.subject_id = subject_id
-        self.session_id = session_id
-        self.measurement_id = measurement_id
-        self.contact_id = contact_id
-        self.session_group = self.table.root.__getattr__(self.subject_id).__getattr__(self.session_id)
-        self.contact_group = self.session_group.__getattr__(self.measurement_id).__getattr__(self.contact_id)
-
-    def create_data(self, data):
-        atom = tables.Atom.from_dtype(data.dtype)
-        filters = tables.Filters(complib="blosc", complevel=9)
-        data_array = self.table.createCArray(where=self.contact_group, name=self.contact_id,
-                                             atom=atom, shape=data.shape, filters=filters)
-        data_array[:] = data
-
-class NormalizedContactDataTable(Table):
-    def __init__(self, subject_id, session_id, measurement_id, contact_id):
-        super(NormalizedContactDataTable, self).__init__()
-        self.subject_id = subject_id
-        self.session_id = session_id
-        self.measurement_id = measurement_id
-        self.contact_id = contact_id
-        self.session_group = self.table.root.__getattr__(self.subject_id).__getattr__(self.session_id)
-        self.contact_group = self.session_group.__getattr__(self.measurement_id).__getattr__(self.contact_id)
-
-    def create_data(self, data):
-        atom = tables.Atom.from_dtype(data.dtype)
-        filters = tables.Filters(complib="blosc", complevel=9)
-        data_array = self.table.createCArray(where=self.contact_group, name=self.contact_id,
-                                             atom=atom, shape=data.shape, filters=filters)
-        data_array[:] = data
+    table.flush()
