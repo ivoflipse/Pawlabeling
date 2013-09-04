9527f038056a5662e3f3f0a1ada910dca85ca2ba
Ivo Flipse <ivoflipse@gmail.com>
2013-09-04 20:16:13 +0200

    Made some changes to the io.load tests, since they were severely broken. The rest still doesn't work, but they are mostly redundant now anyway

--- pawlabeling/tests/functions/testio.py
+++ pawlabeling/tests/functions/testio.py
@@ -11,7 +11,9 @@ class TestLoad(TestCase):
         parent_folder = os.path.dirname(os.path.abspath(__file__))
         file_location = "files\\rsscan_export.zip"
         file_name = os.path.join(parent_folder, file_location)
-        data = io.load(file_name=file_name)
+        with open(file_name, "r") as infile:
+            input_file = infile.read()
+        data = io.load(input_file)
         self.assertEqual(data.shape, (256L, 63L, 250L))
 
     def test_load_empty_file_name(self):
@@ -22,14 +24,18 @@ class TestLoad(TestCase):
         parent_folder = os.path.dirname(os.path.abspath(__file__))
         file_location = "files\\rsscan_export.zip.pkl"
         file_name = os.path.join(parent_folder, file_location)
-        data = io.load(file_name=file_name)
+        with open(file_name, "r") as infile:
+            input_file = infile.read()
+        data = io.load(input_file)
         self.assertEqual(data, None)
 
     def test_load_incorrect_file(self):
         parent_folder = os.path.dirname(os.path.abspath(__file__))
         file_location = "files\\fake_export.zip"
         file_name = os.path.join(parent_folder, file_location)
-        data = io.load(file_name=file_name)
+        with open(file_name, "r") as infile:
+            input_file = infile.read()
+        data = io.load(input_file)
         self.assertEqual(data, None)
 
     # Disabled this one, because its rather slow
@@ -67,7 +73,9 @@ class TestFixOrientation(TestCase):
         parent_folder = os.path.dirname(os.path.abspath(__file__))
         file_location = "files\\rsscan_export.zip"
         file_name = os.path.join(parent_folder, file_location)
-        data = io.load(file_name=file_name)
+        with open(file_name, "r") as infile:
+            input_file = infile.read()
+        data = io.load(input_file)
         new_data = io.fix_orientation(data=data)
         equal = np.array_equal(data, new_data)
         self.assertEqual(equal, True)
@@ -76,7 +84,9 @@ class TestFixOrientation(TestCase):
         parent_folder = os.path.dirname(os.path.abspath(__file__))
         file_location = "files\\rsscan_export.zip"
         file_name = os.path.join(parent_folder, file_location)
-        data = io.load(file_name=file_name)
+        with open(file_name, "r") as infile:
+            input_file = infile.read()
+        data = io.load(input_file)
         # Reverse the plate around the longitudinal axis
         reversed_data = np.rot90(np.rot90(data))
         new_data = io.fix_orientation(data=reversed_data)
