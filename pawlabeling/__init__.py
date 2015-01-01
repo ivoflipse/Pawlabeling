c883d0442b9a7ace8e325f3684b0b98dd3eaa689
Ivo Flipse <ivoflipse@gmail.com>
2015-01-01 13:55:03 +0100

    Update __init__.py
    Bumped the version

--- pawlabeling/__init__.py
+++ pawlabeling/__init__.py
@@ -3,6 +3,6 @@ contact Labeling is a tool to process (veterinary) pressure measurements.
 ===================================================================
 """
 
-__version__ = '0.2'
+__version__ = '0.3'
 
 __all__ = ['functions', 'models', 'tests', 'settings', 'widgets',]
